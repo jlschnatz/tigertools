6796f78b57014e042e1fd541c9319e0df5303ef7
Jan Luca Schnatz <schnatz@psych.uni-frankfurt.de>
2025-04-17 12:30:37 +0200

    update db now only looks for .md files using regex

--- R/update.R
+++ R/update.R
@@ -8,22 +8,15 @@
 #' @export 
 update_db <- function(md_file, item_folder = "items", csv_file = "data_item_tiger.csv", sqlite_file = "db_item.sqlite") {
     validate_item(md_file)
-    df_item <- do.call(rbind, lapply(list_files(file.path(getwd(), item_folder), full.names = TRUE), parse_md_to_csv))
+    files <- list.files(item_folder, full.names = TRUE, pattern = "tiger_item_\\d{3}\\.md$")
+    df_item <- do.call(rbind, lapply(files, parse_md_to_csv))
     readr::write_csv(df_item, csv_file)
     cli::cli_alert_info("Neue CSV-Datei beschrieben, siehe {.file {normalizePath(csv_file)}}")
     con <- DBI::dbConnect(drv = RSQLite::SQLite(), sqlite_file)
     DBI::dbWriteTable(con, "item_db", df_item, overwrite = TRUE)
     DBI::dbDisconnect(con)
-    cli::cli_alert_info("Data was written to database, see {.file {normalizePath(sqlite_file)}}")
+    cli::cli_alert_info("Datei wurde zur Datenbankhinzugefügt, siehe {.file {normalizePath(sqlite_file)}}")
     cli::cli_alert_success("Fertig!")
 }
 
-#' @title List Files in Directory (without subdirectories)
-#' @description List all files in a directory, excluding subdirectories
-#' @param path path to the directory
-#' @return A character vector of file paths
-list_files <- function(path) {
-    files <- list.files(path, full.names = TRUE)
-    files <- files[!file.info(files)$isdir]
-    return(files)
-}
+
