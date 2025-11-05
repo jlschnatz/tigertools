5801b998dafb01a767cc2a374a51dde84904f9bd
Schnatz <janluca.schnatz@gmx.de>
2025-11-05 09:34:55 +0100

    update tests

--- tests/testthat/test-items.R
+++ tests/testthat/test-items.R
@@ -1,8 +1,8 @@
 test_that("Formatting of items is fine!", {
   temp_dir <- tempdir()
   unzip("testdata/mock.zip", exdir = temp_dir)
-  setwd(file.path(temp_dir, "items"))
-  files <- list.files(pattern = "\\.md$", full.names = TRUE)
+  setwd(temp_dir)
+  files <- list.files("items/", pattern = "\\.md$", full.names = TRUE)
   sapply(files, function(x) {
     expect_true(suppressMessages(validate_item(x)))
   })
